8d39d0a7557fcefa2f10e2af6788424b7d8484c8
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-25 15:39:29 -0300

    feat: add of html calculator structure

---
+++ src/components/Calculator.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+  //import { ref } from 'vue';
+
+  //const count = ref<number>(10);
+</script>
+
+<template>
+  <div class="calculator-container">
+    <div class="input-numbers">399,981</div>
+    <div class="buttons-operators">
+      <div class="button-column">
+        <div>7</div>
+        <div>4</div>
+        <div>1</div>
+        <div>,</div>
+      </div>
+      <div class="button-column">
+        <div>8</div>
+        <div>5</div>
+        <div>2</div>
+        <div>0</div>
+      </div>
+      <div class="button-column">
+        <div>9</div>
+        <div>6</div>
+        <div>3</div>
+        <div>/</div>
+      </div>
+      <div class="button-column">
+        <div>DEL</div>
+        <div>+</div>
+        <div>-</div>
+        <div>x</div>
+      </div>
+    </div>
+    <div class="buttons-controllers">
+      <div class="reset">RESET</div>
+      <div class="result">=</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+</style>
