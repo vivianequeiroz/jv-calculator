bf202fe4834e76306471f6efe49cdf9f7336b0b7
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-25 15:38:54 -0300

    chore: clean structure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue';
+  import Calculator from './components/Calculator.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>JV Calculadora</h1>
+  <Calculator/>
 </template>
 
 <style>
